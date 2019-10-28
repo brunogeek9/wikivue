057eb68e9302b5cf9ae23f600376fcaa229b10d1
brunogeek9 <brunogeek9@gmail.com>
2019-10-28 12:43:17 -0300

    adicionando axios e vuex ao projeto

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
-  <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <h1>bem vindo</h1>
+  <div class="container">
+    <header>
+      <h1>{{title}}</h1>
+      <input
+        type="text"
+        placeholder="Search from wikipedia"
+        autofocus
+        v-model="query"
+        v-on:input="getArticles"
+      />
+    </header>
   </div>
 </template>
 
@@ -10,12 +17,31 @@
 // import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     // HelloWorld
+  },
+  computed:{
+    
+  },
+  data() {
+    return {
+      title: "Wikipedia Viewer"
+    };
   }
-}
+};
 </script>
 
 <style>
+.container {
+  max-width: 760px;
+  margin: 0 auto;
+  padding: 2em;
+}
+h1 {
+  text-align: center;
+}
+main > header {
+  margin-bottom: 4em;
+}
 </style>
